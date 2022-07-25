5a9309d0c36f4834af2c34a6dc07741dcae5c67f
Zomato1 <106505455+Zomato1@users.noreply.github.com>
2022-07-25 11:11:39 +0530

    Update forcesub.py

--- TeamTeleRoid/forcesub.py
+++ TeamTeleRoid/forcesub.py
@@ -47,7 +47,7 @@ async def ForceSub(bot: Client, cmd: Message):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Somthing Went Wrong. Contact My [Owner](https://t.me/RoyalKrrishna).",
+            text="Somthing Went Wrong. Contact My [Owner](https://t.me/KGN_OFFICIAL).",
             parse_mode="markdown",
             disable_web_page_preview=True
         )
